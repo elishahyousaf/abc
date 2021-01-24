53d80f62a177fb0e7a5ee1aed6644f2baf8bd71f
Elishah Yousaf <elishahroy@gmail.com>
2021-01-24 21:03:21 +0500

    Add files via Upload

---
+++ 6-My+First+Project/My First Project/My First Project/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  My First Project
+//
+//  Created by Amir J on 8/29/18.
+//  Copyright © 2018 Amir J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 6-My+First+Project/My First Project/My First Project/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  My First Project
+//
+//  Created by Amir J on 8/29/18.
+//  Copyright © 2018 Amir J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
